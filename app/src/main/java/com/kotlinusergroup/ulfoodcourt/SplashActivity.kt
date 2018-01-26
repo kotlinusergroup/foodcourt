84a4d851c92bdbaa49cb9c1102d7fad4fca262a5
ajithvgiri <mail.ajithvgiri@gmail.com>
2018-01-26 23:16:25 +0530

    layout and firebase init commit

--- app/src/main/java/com/kotlinusergroup/ulfoodcourt/SplashActivity.kt
+++ app/src/main/java/com/kotlinusergroup/ulfoodcourt/SplashActivity.kt
@@ -1,7 +1,15 @@
 package com.kotlinusergroup.ulfoodcourt
 
+import android.content.Intent
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.kotlinusergroup.ulfoodcourt.activities.MenuActivity
 
 class SplashActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MenuActivity::class.java))
+        finish()
+    }
 }
---
+++ app/src/main/java/com/kotlinusergroup/ulfoodcourt/activities/MenuActivity.kt
@@ -0,0 +1,13 @@
+package com.kotlinusergroup.ulfoodcourt.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.kotlinusergroup.ulfoodcourt.R
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
